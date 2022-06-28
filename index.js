fc995d5ab57fb88495380244a20e3971388ee826
devHDK <hyundo91082@gmail.com>
2022-06-28 21:11:59 +0900

    Login 구현

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000;
 const { User } = require("./model/User");
 const bodyParser = require("body-parser");
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({ extended: true })); // url parser
 app.use(bodyParser.json());
@@ -17,16 +18,53 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 안녕하세요");
+  res.send("/register , /login, /auth ");
 });
 
 app.post("/register", (req, res) => {
   //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
   const user = new User(req.body);
   user.save((err, doc) => {
+    //error 처리
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 email을 DB에서 찾음
+
+  //비밀 번호까지 맞다면 토큰 생성하기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    //요청된 email이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    console.log(req.body.password);
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+
+      user.generateToken((err, user) => {
+        console.log("gen Token");
+
+        if (err) return res.status(400).send(err);
+
+        //token을 저장한다..
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
     });
   });
 });
